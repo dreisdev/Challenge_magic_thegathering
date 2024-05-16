d59575fb7550f7dbd0c6179f63d389b2ef7e4eb6
Diego Reis <dreis.dev@outlook.com>
2024-05-16 03:47:53 -0300

    config app - route.

--- magic-cards/src/app/app.component.ts
+++ magic-cards/src/app/app.component.ts
@@ -3,19 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import { FilterCardsComponent } from './components/filter-cards/filter-cards.component';
 import { SearchCardsComponent } from './components/search-cards/search-cards.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CardDisplayComponent } from './components/card-display/card-display.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, FilterCardsComponent, SearchCardsComponent],
+  imports: [RouterOutlet, HttpClientModule, FilterCardsComponent, SearchCardsComponent, CardDisplayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'magic-cards';
   searchFilters: any;
+  boosterCards: any[] = [];
 
   onSearch(filters: any) {
     this.searchFilters = filters;
+  }
+
+  onBoosterOpened(cards: any[]): void {
+    this.boosterCards = cards;
   }
 }
--- magic-cards/src/app/app.routes.ts
+++ magic-cards/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CardDisplayComponent } from './components/card-display/card-display.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'cards',component: CardDisplayComponent}
+];
