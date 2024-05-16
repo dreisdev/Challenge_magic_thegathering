d6e6b343b9a3d8bdb3d5570af5f548f6fc1c870e
Diego Reis <dreis.dev@outlook.com>
2024-05-16 03:47:12 -0300

    button booster.

--- magic-cards/src/app/components/search-cards/search-cards.component.ts
+++ magic-cards/src/app/components/search-cards/search-cards.component.ts
@@ -1,22 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from '../../api.service';
 import { FilterCardsComponent } from '../filter-cards/filter-cards.component';
+import { LoadingComponent } from '../loading/loading.component';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-search-cards',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, FilterCardsComponent],
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, FilterCardsComponent, LoadingComponent],
   templateUrl: './search-cards.component.html',
   styleUrl: './search-cards.component.scss'
 })
 export class SearchCardsComponent implements OnInit, OnChanges {
   @Input() filters: any;
   sets: any[] = [];
+  cards: any[] = [];
+  loading: boolean = false;
+  error: string = '';
 
-  constructor(private apiService: ApiService) {}
+  @Output() boosterOpened: EventEmitter<any> = new EventEmitter(); 
+
+  constructor(private apiService: ApiService, private router: Router) {}
 
   ngOnInit(): void {}
 
@@ -33,5 +41,40 @@ export class SearchCardsComponent implements OnInit, OnChanges {
       this.sets = data.sets;
     });
   }
+
+  openBoosters(setCode: string): void {
+    this.loading = true;
+    this.cards= [];
+    this.error = '';
+    let count = 0;
+ 
+
+  const fetchBooster = () => {
+    this.apiService.getBooster(setCode).subscribe((data: any)=> {
+      console.log('Data from API call Booster:', data);
+      const creatureCards = data.cards.filter((card: any)=> card.types.includes('Creature'));
+      this.cards.push(...creatureCards);
+
+      if (this.cards.length < 30 && count < 5) {
+        count++;
+        fetchBooster();
+      } else {
+        this.loading = false;
+        this.boosterOpened.emit(this.cards.slice(0, 30));
+        this.router.navigate(['/cards'], { state: { cards: this.cards.slice(0, 30) } });
+      }
+    },
+    (error) => {
+      if (error.status === 400) {
+        this.error = 'Nenhum booster encontrado para este conjunto.';
+      } else {
+        this.error = 'Erro ao abrir os boosters. Tente novamente.';
+      }
+      this.loading = false;
+    }
+  );
+  }
+  fetchBooster();
+}
   
 }
